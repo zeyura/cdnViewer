ae8d045375d2e88b8d5f267a2801db14079a5f8c
zeyura <zeyura75@gmail.com>
2021-01-23 12:04:32 +0200

    вставил форму поиска..

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@
     <v-app-bar
       app
       color="primary"
-      dark
     >
       <div class="d-flex align-center">
         <v-img
@@ -15,17 +14,15 @@
           width="40"
         />
 
-
       </div>
 
       <v-spacer></v-spacer>
 
-
     </v-app-bar>
 
-    <v-main>
+    <v-main >
 
-      <Home />
+        <Home />
 
     </v-main>
   </v-app>
@@ -46,3 +43,12 @@ export default {
   }),
 };
 </script>
+
+<style lang="scss" scoped>
+
+  .v-main {
+    width: 700px;
+    margin: auto;
+  }
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,63 @@
 <template>
-  <div class="home">
+    <div class="cdn-box">
 
-<h1>Hello</h1>
+        <h1 class="page-title">CDN Packets</h1>
 
-  </div>
+        <form>
+            <v-text-field
+                    v-model="name"
+                    :error-messages="nameErrors"
+                    label="enter name"
+                    required
+            >
+            </v-text-field>
+
+            <v-btn
+                    class="mr-4"
+                    @click="submit"
+            >
+                submit
+            </v-btn>
+            <v-btn @click="clear">
+                clear
+            </v-btn>
+        </form>
+
+        <ul class="packets-list">
+
+        </ul>
+
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-//import HelloWorld from '@/components/HelloWorld.vue'
+    // @ is an alias to /src
+    //import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-    name: 'Home',
-    components: {
+    export default {
+        name: 'Home',
+        components: {},
+        data: () => ({}),
+        async mounted() {
 
-    },
-    data: () => ({
+        },
+        computed: {},
+        methods: {}
+    }
+</script>
 
-    }),
-    async mounted() {
+<style lang="scss" scoped>
 
-    },
-    methods: {
+    .page-title {
+        margin-bottom: 25px;
+    }
+
+    .cdn-box {
+        margin-top: 100px;
+    }
+
+    .packets-list {
 
     }
-}
-</script>
+
+</style>
